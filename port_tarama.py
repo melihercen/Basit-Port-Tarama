7b1440c569525da0ccb270cdc69660f004db4954
melihercen <127030992+melihercen@users.noreply.github.com>
2024-11-04 16:57:09 +0300

    Add files via upload

---
+++ port_tarama.py
@@ -0,0 +1,42 @@
+import os
+
+os.system("apt-get install figlet")
+os.system("clear")
+os.system("figlet PORT TARAMA")
+
+print("""
+1-Hizli Tarama
+2-Detaylı Tarama
+3-Belirtilen Port Tarma
+""")
+
+secim=int(input("Şeçiminiz: "))
+if secim==1:
+    adres=input("hedef adress ip veya alan adı: ")
+    os.system(f"nmap {adres}")
+
+elif secim==2:
+    adres=input("hedef adress ip veya alan adı: ")
+
+    os.system(f"nmap -A -sV -vv -p- sS -O {adres}")
+
+elif secim==3:
+    adres=input("hedef adress ip veya alan adı: ")
+    port=int(input("Taramak istediğiniz port:"))
+    print("""
+1-Hizli
+2-Detaylı
+          """)
+    secim2=int(input("Seçim:"))
+    if secim2==1:
+        os.system(f"nmap -p {port} {adres}")
+    elif secim2==2:
+        os.system(f"nmap -p {port} -sV -sS -A -vv {adres}")
+    else:
+        print("Hatalı Secim Cıkış yapılıyor....")
+        quit()
+
+else:
+    print("Hatalı Seçim Cıkıs yapılıyor...")
+    quit()
+
